3102a8de53349ef85251342d52b4c20811ec852f
Tiofx <ioracle2718@gmail.com>
2019-03-16 14:05:22 +0300

    [add] taks for generating .tex -> .pdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,41 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("guru.nidi:graphviz-java:0.8.3")
     implementation("ch.qos.logback:logback-classic:1.2.3")
+
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks {
+    val RESOURCES_FOLDER = rootProject.projectDir
+            .resolve("src")
+            .resolve("main")
+            .resolve("resources")
+
+    val run by existing
+
+    val texToPdf by registering {
+        val fileName = "report.tex"
+
+        doFirst {
+            exec {
+                workingDir = RESOURCES_FOLDER.absoluteFile
+                isIgnoreExitValue = true
+
+                commandLine("xelatex", "-interaction=nonstopmode", "--shell-escape", "--file-line-error", fileName)
+            }
+        }
+    }
+
+    val makeReport by registering {
+        dependsOn(texToPdf)
+    }
+
+    val runAndMakeReport by registering {
+        dependsOn(run)
+        dependsOn(makeReport)
+    }
+
 }
