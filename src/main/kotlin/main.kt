d87442600a9427c70bef7434ae12f7e0fbb5b698
Tiofx <ioracle2718@gmail.com>
2019-03-21 21:47:05 +0300

    [move] main from root to report.latex
    [change] original main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,4 @@
-import algorithm.*
-import report.latex.LatexConverter
-import report.latex.TwoAppendixTemplate
-
 fun main() {
     report.graphviz.main()
-
-    val program = programText
-            .run { prepareToLatex() }
-            .map(::Operator)
-            .let { CashedProgram(it) }
-
-
-    val cpf = CPF(program)
-    val result = cpf.form()
-
-    val latex = LatexConverter(result).form()
-    TwoAppendixTemplate(latex).save()
+    report.latex.main()
 }
