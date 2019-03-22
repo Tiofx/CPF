475ad05b030d608ce5fcb5ceff3ad5bf572f1767
Tiofx <ioracle2718@gmail.com>
2019-03-22 15:38:22 +0300

    [add] parses for CPF.Iteration to graphviz representation

--- src/main/kotlin/report/graphviz/iteration.kt
+++ src/main/kotlin/report/graphviz/iteration.kt
@@ -1,5 +1,44 @@
 package report.graphviz
+
+import algorithm.CPF
+import algorithm.RESOURCES_FOLDER
+import guru.nidi.graphviz.engine.Format
+import guru.nidi.graphviz.engine.Graphviz
+import report.OperatorNamer
+import report.toPlainString
 import kotlin.math.min
+
+
+class CPFItreationsGraphSaver(val iterations: List<CPF.Iteration>) {
+    private val programIndexes = ReducedProgram()
+    private val namer = OperatorNamer(iterations)
+
+    fun save() {
+        parse().forEachIndexed { i, it ->
+            Graphviz.fromString(it)
+                    .render(Format.PNG)
+                    .toFile(RESOURCES_FOLDER
+                            .resolve("iterations")
+                            .toAbsolutePath()
+                            .resolve("${i + 1}.png")
+                            .toFile())
+        }
+    }
+
+    private fun parse() = iterations.map { parse(it) }
+
+    private fun parse(cpfIteration: CPF.Iteration): String {
+        programIndexes.iteration = cpfIteration
+
+        return programIndexes.parse().run {
+            fun Int.toName() = namer.name(cpfIteration.program, this).toPlainString()
+            fun IntRange.toOperator() = map { SingleOperator(it.toName()) }
+
+            toGraph(IntRange::toOperator)
+        }
+    }
+}
+
 class ReducedProgram(val limit: Int = 15,
 //                     val groupMinSize: Int = 3,
                      val startMinSize: Int = 2,
