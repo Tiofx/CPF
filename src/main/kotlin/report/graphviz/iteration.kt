b064a1c0b71335ec1e74ef0d09885972bfc0f761
Tiofx <ioracle2718@gmail.com>
2019-03-22 15:36:44 +0300

    [add] graphviz node representation
    single node, skip, node group

--- src/main/kotlin/report/graphviz/iteration.kt
+++ src/main/kotlin/report/graphviz/iteration.kt
@@ -48,3 +48,25 @@ class ReducedProgram(val limit: Int = 15,
 
     private fun remNumber(min: Int, max: Int) = if (max < min) min - max else 0
 }
+interface Node {
+    fun toGraphviz(): String
+}
+
+class SingleOperator(val name: String) : Node {
+    override fun toGraphviz(): String = name
+}
+
+class NodeSkip : Node {
+    override fun toGraphviz(): String = "$this[label='...']"
+}
+
+class NodeGroup(val nodeSequence: List<Node>) : Node {
+    override fun toGraphviz(): String = """
+        |subgraph cluster_group {
+        |    penwidth = 2
+        |    margin = 10
+        |
+        |   ${nodeSequence.joinToString(" ", transform = Node::toGraphviz)}
+        |}
+    """.trimMargin()
+}
