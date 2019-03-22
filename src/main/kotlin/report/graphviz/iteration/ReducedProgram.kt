7758925ffec30d0e6e4c4810fdb8ade324869503
Tiofx <ioracle2718@gmail.com>
2019-03-22 16:38:24 +0300

    [fix] in reduced program logic

--- src/main/kotlin/report/graphviz/iteration/ReducedProgram.kt
+++ src/main/kotlin/report/graphviz/iteration/ReducedProgram.kt
@@ -22,14 +22,14 @@ class ReducedProgram(val limit: Int = 15,
     val groupPreferredSize get() = limit - (startMinSize + endMinSize) + remFromStart + remFromEnd
 
     val groupSize get() = min(groupPreferredSize, groupMaxSize)
-    val startSize get() = (limit - groupSize) / 2 + (limit - groupSize) % 2
-    val endSize get() = limit - (startSize + groupSize)
+    val startSize get() = min((limit - groupSize) / 2 + (limit - groupSize) % 2, group.start)
+    val endSize get() = min(limit - (startSize + groupSize), program.lastIndex - group.endInclusive)
 
     val startRange get() = if (startSize > 0) 0..(startSize - 1) else IntRange.EMPTY
-    val groupStartRange get() = group.start..(group.start + groupSize / 2 + groupSize % 2 - 1)
     val groupSkipAdditionalSpace get() = if (groupMaxSize - groupSize == 1) 1 else 0
-    val groupEndRange get() = (group.endInclusive - groupSize / 2 + 1 + groupSkipAdditionalSpace)..group.endInclusive
-    val endRange get() = if (endSize > 0) (groupEndRange.endInclusive + 1)..program.lastIndex else IntRange.EMPTY
+    val groupStartRange get() = group.start..(group.start + groupSize / 2 + groupSize % 2 - 1 - groupSkipAdditionalSpace)
+    val groupEndRange get() = (group.endInclusive - groupSize / 2 + 1)..group.endInclusive
+    val endRange get() = if (endSize > 0) (program.lastIndex - endSize + 1)..program.lastIndex else IntRange.EMPTY
 
     fun parse() = Indexes(
             startRange,
@@ -44,9 +44,9 @@ class ReducedProgram(val limit: Int = 15,
             val groupEnd: IntRange,
             val end: IntRange
     ) {
-        val hasSkipBeforeGroup: Boolean = !start.isEmpty() && start.endInclusive + 1 == groupStart.start
-        val hasSkipInGroup: Boolean = groupStart.endInclusive + 1 == groupEnd.start
-        val hasSkipBeforeEnd: Boolean = !end.isEmpty() && groupEnd.endInclusive + 1 == end.start
+        val hasSkipBeforeGroup: Boolean = !start.isEmpty() && start.endInclusive + 1 != groupStart.start
+        val hasSkipInGroup: Boolean = !groupStart.isEmpty() && groupStart.endInclusive + 1 != groupEnd.start
+        val hasSkipBeforeEnd: Boolean = !end.isEmpty() && groupEnd.endInclusive + 1 != end.start
     }
 
     private fun remNumber(min: Int, max: Int) = if (max < min) min - max else 0
