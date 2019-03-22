fef83cb5607fb45440d54838332dfbc341b6608b
Tiofx <ioracle2718@gmail.com>
2019-03-22 16:38:12 +0300

    [add] node name attribute
    some change in name format
    [change] skip node border color to invisible

--- src/main/kotlin/report/graphviz/iteration/graphviz_representation.kt
+++ src/main/kotlin/report/graphviz/iteration/graphviz_representation.kt
@@ -1,24 +1,31 @@
 package report.graphviz.iteration
 
 interface Node {
+    val nodeName: String
     fun toGraphviz(): String
 }
 
 class SingleOperator(val name: String) : Node {
-    override fun toGraphviz(): String = name
+    override val nodeName: String get() = "\"$name\""
+
+    override fun toGraphviz(): String = "\"$name\""
 }
 
 class NodeSkip : Node {
-    override fun toGraphviz(): String = "$this[label='...']"
+    override val nodeName: String get() = "\"${hashCode()}\"[label=\"...\", color=invis]"
+
+    override fun toGraphviz(): String = "\"${hashCode()}\""
 }
 
 class NodeGroup(val nodeSequence: List<Node>) : Node {
+    override val nodeName: String get() = TODO("not implemented")
+
     override fun toGraphviz(): String = """
         |subgraph cluster_group {
         |    penwidth = 2
         |    margin = 10
         |
-        |   ${nodeSequence.joinToString(" ", transform = Node::toGraphviz)}
+        |   ${nodeSequence.joinToString(" ", transform = Node::nodeName)}
         |}
     """.trimMargin()
 }
